1bda87a1d2e0aa7c1f140dcc5466d4a0a352e46f
Rodier <francisrodier78@yahoo.fr>
2020-08-26 17:01:53 +0200

    Formulaires

---
+++ src/Controller/AdController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Permet de créer une annonce
+     * 
+     * @Route("/ads/new", name="ads_create")
+     *
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $manager){
+        $ad = new Ad;
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>Test</strong> a bien été enregistrée."
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     * Utilisation du ParamConvert. 
+     * Le ParamConvert comprend que l'on cherhce un enregistrement Ad 
+     * avec la donnée ({slug}) passée en entrée.
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show(Ad $ad) {
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+}
